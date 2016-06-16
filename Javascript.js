7e04f1314b1a20492316bb7d311fcd759a48522a
MarkPerez <markperez88@hotmail.co.uk>
2016-06-17 00:20:00 +0100

    UpdatedQuiz

--- Javascript.js
+++ Javascript.js
@@ -1,8 +1,16 @@
 
 var event1 = document.getElementById("button1");
-event1.addEventListener('click', submit, false);
+event1.addEventListener('click', next, false);
+
+var event2 = document.getElementById("button2");
+event2.addEventListener('click', submit, false);
 
 var quizQuestions = [];
+
+var quizAnswers = ["C", "B", "A", "C", "B"];
+
+var i = -1;
+var j = -1;
 
 var correctAnswers = 0;
 
@@ -13,15 +21,15 @@ function Question(text, optionA, optionB, optionC){
     this.optionC = optionC;
 } 
 
-var firstQuestion = new Question("What is the closest planet to Earth?", "Mercury", "Venus", "Mars");
+var firstQuestion = new Question("Which is the closest planet to Earth?", "A) Mercury", "B) Venus", "C) Mars");
 
-var secondQuestion = new Question("How many planets are there in the Solar System?", "7", "8", "9");
+var secondQuestion = new Question("How many planets are there in the Solar System?", "A) 7", "B) 8", "C) 9");
 
-var thirdQuestion = new Question("Which is the biggest planet?", "Jupiter", "Saturn", "Neptune");
+var thirdQuestion = new Question("Which is the biggest planet in the Solar System?", "A) Jupiter", "B) Saturn", "C) Neptune");
 
-var fourthQuestion = new Question("Which of these is NOT a moon of Jupiter?", "Ganymede", "Europa", "Titan");
+var fourthQuestion = new Question("Which of these is NOT a moon of Jupiter?", "A) Ganymede", "B) Europa", "C) Titan");
 
-var fifthQuestion = new Question("What is Ceres?", "Moon", "Dwarf Planet", "Asteroid");
+var fifthQuestion = new Question("What is Ceres?", "A) Moon", "B) Dwarf Planet", "C) Asteroid");
 
 quizQuestions.push(firstQuestion);
 quizQuestions.push(secondQuestion);
@@ -31,13 +39,29 @@ quizQuestions.push(fifthQuestion);
 
 //document.getElementById("demo").innerHTML = quizQuestions;
 
-function submit(){
+function next(){
     text = "";
-    for (i = 0; i < quizQuestions.length; i++) {
+    i++;
+    for (; i < quizQuestions.length; ) {
         text += quizQuestions[i].text + "<br>" + quizQuestions[i].optionA + "<br>" + quizQuestions[i].optionB + "<br>" + quizQuestions[i].optionC + "<br>" + "<br>";
         document.getElementById("demo").innerHTML = text;
-        
-        
+        break;
     }
-    
+}
+
+function submit(){
+    j++;
+/*    for (; j < quizAnswers.length; ) {
+        if (document.getElementById("answers").value == quizAnswers[j]) {
+            correctAnswers++;
+            document.getElementById("answers").reset();
+            }
+        else {
+            document.getElementById("answers").reset();
+        }
+        document.getElementById("results").innerHTML = correctAnswers;
+        break;
+    }*/
+    //var input = document.getElementById("answers").radioValue;
+    document.getElementById("results").innerHTML = "test";
 }
